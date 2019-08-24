ea7f1a0ed9398a39d0d429c85f44523736ce6426
Varun Jain <37890928+Varun901@users.noreply.github.com>
2019-08-24 13:59:11 -0400

    Changed Serializer and initial migration

--- Django/hackthe6ix/donor/models.py
+++ Django/hackthe6ix/donor/models.py
@@ -2,16 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class User_(models.Model):
+class Donor(models.Model):
     user = models.OneToOneField(User,
                                 on_delete=models.CASCADE,
                                 primary_key=True,
-                                related_name='user')
-    
-
-class Donor(models.Model):
-    user = models.OneToOneField(User_,
-                                on_delete=models.CASCADE,
-                                primary_key=True,
-                                related_name='user')
-    customerId = models.CharField()
+                                related_name='User')
+    customerId = models.CharField(max_length=50)
--- Django/hackthe6ix/donor/serializers.py
+++ Django/hackthe6ix/donor/serializers.py
@@ -4,4 +4,4 @@ from .models import Donor
 class DonorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Donor
-        fields = ('id', 'name', 'email')
+        fields = ('id', 'user', 'customerId')
