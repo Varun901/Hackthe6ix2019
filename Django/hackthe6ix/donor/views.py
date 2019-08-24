522322453591cfe0b6744192f1f8d69caa8d8afd
bhatts39 <sunnynbhatt@gmail.com>
2019-08-24 16:45:36 -0400

    Commented out things

--- Django/hackthe6ix/donor/views.py
+++ Django/hackthe6ix/donor/views.py
@@ -38,23 +38,23 @@ class PurchaseView(viewsets.ModelViewSet):
     queryset = Purchase.objects.all()
     serializer_class = PurchaseSerializer
 
-class GetPurchases(APIView):
-    def get(self, request):
-            location = request.query_params.location
-            purchases = Purchase.objects.all()
-            purchases.sort(key=distance)
-            return purchases
-        def distance(point):
-            return Math.sqrt(distance[0]**2 - distance[1]**2)
+# class GetPurchases(APIView):
+#     def get(self, request):
+#             location = request.query_params.location
+#             purchases = Purchase.objects.all()
+#             purchases.sort(key=distance)
+#             return purchases
+#         def distance(point):
+#             return Math.sqrt(distance[0]**2 - distance[1]**2)
 
-class Reimburse(APIView):
-    def post(self, request):
-        #todo implementing actually charging a credit card
-        purchaseId = request.data.purchaseId
-        Purchase.objects.get(id=purchaseId)
-        Purchase.donor = request.user
-        Purchase.save()
-        return Response({result:success},status=HTTP_200_OK)
+# class Reimburse(APIView):
+#     def post(self, request):
+#         #todo implementing actually charging a credit card
+#         purchaseId = request.data.purchaseId
+#         Purchase.objects.get(id=purchaseId)
+#         Purchase.donor = request.user
+#         Purchase.save()
+#         return Response({result:success},status=HTTP_200_OK)
 
     # Donor Registration 
     # Donor Login
