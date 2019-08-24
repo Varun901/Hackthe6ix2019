ce60d885c4e8b60e17a61aeebc81dc29137b6bf5
Varun Jain <37890928+Varun901@users.noreply.github.com>
2019-08-24 11:29:10 -0400

    Fixed Django Rest API Root

--- Django/hackthe6ix/donor/urls.py
+++ Django/hackthe6ix/donor/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('donors', views.DonorView)
 
 urlpatterns = [
-    
+    path("", include(router.urls))
 ]
