a53fdf19862cfad21f391521f4fedc0202d61fc0
bhatts39 <sunnynbhatt@gmail.com>
2019-08-24 11:51:11 -0400

    Model changes

--- Django/hackthe6ix/donor/models.py
+++ Django/hackthe6ix/donor/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class User_(models.Model):
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE,
+                                primary_key=True,
+                                related_name='user')
+    
+
 class Donor(models.Model):
-    name = models.CharField(max_length = 50)
-    email = models.CharField(max_length = 50)
+    user = models.OneToOneField(User_,
+                                on_delete=models.CASCADE,
+                                primary_key=True,
+                                related_name='user')
+    customerId = models.CharField()
+
+    
