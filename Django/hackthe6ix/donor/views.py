58d81e0e85e564a085a634cfcbb41c19e9ab342a
Aditya Sharma <22263756+AdityaSharma6@users.noreply.github.com>
2019-08-24 16:32:03 -0400

    Donor Registration View

--- Django/hackthe6ix/donor/views.py
+++ Django/hackthe6ix/donor/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.views import APIView
 from .models import *
 from .serializers import *
 
 # Create your views here.
+
+class DonorRegistration(APIView):
+    def post(self, request, format=None):
+        data = request.DATA
+        username = data.get('username', None)
+        password = data.get('password', None)
+        email = data.get('email', None)
+        first_name = data.get('first name', None)
+        last_name = data.get('last name', None)
+        if (username != None) or (password != None):
+            new_user = User(username=username, password=password, email=email, first_name = first_name, last_name = last_name)
+            new_user.save()
+            new_donor = Donor(user=User.objects.get(username=username))
+            new_donor.save()  
+
+
 class DonorView(viewsets.ModelViewSet):
     queryset = Donor.objects.all()
     serializer_class = DonorSerializer
@@ -19,3 +36,17 @@ class StoreView(viewsets.ModelViewSet):
 class PurchaseView(viewsets.ModelViewSet):
     queryset = Purchase.objects.all()
     serializer_class = PurchaseSerializer
+
+
+    # Donor Registration 
+    # Donor Login
+    # Donor Profile
+    # Recipient Search
+
+    # Recipient Registration
+    # Recipient Login
+    # Recipient Profile
+    
+    # Store Registration
+    # Store Login
+    # Store Profile
