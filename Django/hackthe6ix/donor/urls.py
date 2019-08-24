52bcbd4671b0b812cc9c16d5a5c375e0906b2506
Varun Jain <37890928+Varun901@users.noreply.github.com>
2019-08-24 19:54:06 -0400

    Added Reimburse View

--- Django/hackthe6ix/donor/urls.py
+++ Django/hackthe6ix/donor/urls.py
@@ -4,10 +4,14 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('donors', views.DonorView)
+router.register('users', views.UserView)
 router.register('recipients', views.RecipientView)
 router.register('stores', views.StoreView)
 router.register('purchases', views.PurchaseView)
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path('donor/reimburse', views.Reimburse.as_view())
 ]
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
--- Django/hackthe6ix/donor/views.py
+++ Django/hackthe6ix/donor/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import *
 from .serializers import *
@@ -38,6 +38,10 @@ class PurchaseView(viewsets.ModelViewSet):
     queryset = Purchase.objects.all()
     serializer_class = PurchaseSerializer
 
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
 # class GetPurchases(APIView):
 #     def get(self, request):
 #             location = request.query_params.location
@@ -47,14 +51,25 @@ class PurchaseView(viewsets.ModelViewSet):
 #         def distance(point):
 #             return Math.sqrt(distance[0]**2 - distance[1]**2)
 
-# class Reimburse(APIView):
-#     def post(self, request):
-#         #todo implementing actually charging a credit card
-#         purchaseId = request.data.purchaseId
-#         Purchase.objects.get(id=purchaseId)
-#         Purchase.donor = request.user
-#         Purchase.save()
-#         return Response({result:success},status=HTTP_200_OK)
+class Reimburse(APIView):
+    def post(self, request, format=None):
+        #todo implementing actually charging a credit card
+        user = User.objects.get(username=request.user.username)
+        purchaseId = request.data.get('purchaseId', None)
+        Pur = Purchase.objects.get(id=purchaseId)
+        Don = Donor.objects.get(user=User.objects.get(id=1))
+        # Don = Donor.objects.get(user=request.user) <- Needs to work
+        Don.total_reimbursements_made += 1
+        Don.total_reimbursements_value += Pur.purchase_value
+        Don.save()
+        Rec = Recipient.objects.get(user=Pur.recipient)
+        Rec.total_reimbursements_value += Pur.purchase_value
+        Rec.total_reimbursements_accepted += 1
+        Rec.save()
+        Pur.doner = Don
+        # Pur.donor = request.user
+        Pur.save()
+        return Response({"result":"success"},status=status.HTTP_200_OK)
 
     # Donor Registration 
     # Donor Login
