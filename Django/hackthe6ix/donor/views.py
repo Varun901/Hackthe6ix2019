bda06d76168ae4778f800b27f1ac489ff8fe30ff
bhatts39 <sunnynbhatt@gmail.com>
2019-08-24 16:40:17 -0400

    Added views

--- Django/hackthe6ix/donor/views.py
+++ Django/hackthe6ix/donor/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import *
 from .serializers import *
 
@@ -19,3 +21,21 @@ class StoreView(viewsets.ModelViewSet):
 class PurchaseView(viewsets.ModelViewSet):
     queryset = Purchase.objects.all()
     serializer_class = PurchaseSerializer
+
+class GetPurchases(APIView):
+    def get(self, request):
+            location = request.query_params.location
+            purchases = Purchase.objects.all()
+            purchases.sort(key=distance)
+            return purchases
+        def distance(point):
+            return Math.sqrt(distance[0]**2 - distance[1]**2)
+
+class Reimburse(APIView):
+    def post(self, request):
+        #todo implementing actually charging a credit card
+        purchaseId = request.data.purchaseId
+        Purchase.objects.get(id=purchaseId)
+        Purchase.donor = request.user
+        Purchase.save()
+        return Response({result:success},status=HTTP_200_OK)
