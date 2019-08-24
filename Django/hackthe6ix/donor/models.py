91852e5eaa58e04c52892b178b950e2f457efe02
bhatts39 <sunnynbhatt@gmail.com>
2019-08-24 14:45:18 -0400

    models stuff

--- Django/hackthe6ix/donor/models.py
+++ Django/hackthe6ix/donor/models.py
@@ -6,5 +6,28 @@ class Donor(models.Model):
     user = models.OneToOneField(User,
                                 on_delete=models.CASCADE,
                                 primary_key=True,
-                                related_name='User')
+                                related_name='Donor')
     customerId = models.CharField(max_length=50)
+
+class Recipient(models.Model):
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE,
+                                primary_key=True,
+                                related_name='Recipient')
+
+class Store(models.Model):
+    id = models.AutoField(primary_key=True)
+
+class Purchase(models.Model):
+    id = models.AutoField(primary_key=True)
+    recipient = models.ForeignKey(Recipient,
+                            on_delete=models.SET_NULL,
+                            null=True)
+    donor = models.ForeignKey(Donor,
+                            on_delete=models.SET_NULL,
+                            null=True)
+    store = models.ForeignKey(Store,
+                            on_delete=models.SET_NULL,
+                            null=True)
+
+
