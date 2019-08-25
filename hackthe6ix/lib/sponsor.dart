66eeb68a7287cec3637196c290d3096e9de54929
Adam Yu <adamxhy123@gmail.com>
2019-08-25 02:26:02 -0400

    google maps works

--- hackthe6ix/lib/sponsor.dart
+++ hackthe6ix/lib/sponsor.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class Sponsor extends StatelessWidget {
+class Sponsor extends StatefulWidget {
+  @override
+  _SponsorState createState() => _SponsorState();
+}
+
+class _SponsorState extends State<Sponsor> {
+  GoogleMapController mapController;
+  LatLng center;
+
+  @override
+  void initState() {
+    super.initState();
+    center = LatLng(45.521563, -122.677433);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Placeholder(),
+      body: GoogleMap(
+        onMapCreated: (mapController) => mapController = mapController,
+        initialCameraPosition: CameraPosition(target: center, zoom: 11),
+      ),
     );
   }
 }
